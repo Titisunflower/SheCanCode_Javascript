8057a6967252a06182b9ba930ab05b0991c9c623
Titisunflower <henuwz@gmail.com>
2023-11-21 23:19:34 +0200

    Practice and Notes

---
+++ Practice/practice.js
@@ -0,0 +1,8 @@
+let myName = "Henriette";
+const surName = "Uwizeyimana";
+let fullNames = myName + surName;
+console.log(fullNames);
+
+// = assign
+// == comparison on values only
+// === comparison on both  values and Datatypes.
