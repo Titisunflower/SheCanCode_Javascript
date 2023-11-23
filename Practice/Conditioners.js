b9ec7fa102bf2e91e399f2a69bc4afa946bb04b0
Titisunflower <henuwz@gmail.com>
2023-11-23 16:04:46 +0200

    Conditioners

---
+++ Practice/Conditioners.js
@@ -0,0 +1,33 @@
+let temperature = 75;
+console.log(`The temperature is ${temperature}`);
+ 
+if (temperature > 70) {
+   console.log("It's warm outside");
+}
+
+let numero = 10;
+ 
+if (numero >= 10) {
+   numero = numero + 5;
+}
+ 
+console.log(numero);
+
+let test;
+ 
+if (false) {
+   test = 'test';
+}
+ 
+console.log(test);
+let num = 0;
+if (1 < 2) {
+   num = 1;
+}
+console.log(num === 1);
+
+let text = 'hello';
+if (text === 'hello') {
+   text = text + ' world';
+}
+console.log(text === 'hello world');
