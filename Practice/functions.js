6ecdd9da5274c46f090bbb131bc548da3d1a74f9
Titisunflower <henuwz@gmail.com>
2023-11-27 15:26:47 +0200

    Functions

---
+++ Practice/functions.js
@@ -0,0 +1,42 @@
+function logMessage() {
+    console.log('Hello world!');
+  }
+   
+  logMessage();
+
+  logMessage();
+
+  logMessage();
+  //let's compare two ways to create functions.
+  
+  function logMessageOne() {
+    console.log('How are you?');
+  }
+   
+  const logMessageTwo = function() {
+    console.log('Great, thanks!');
+  }
+   
+  logMessageOne();
+   
+  logMessageTwo();
+
+ // They are similar to each other
+ 
+ function func() {
+    return 5;
+  }
+   
+  const result = func();
+   
+  console.log(result);
+  //expected value is 5
+
+  function func() {
+    const num = 5;
+  }
+   
+  const result = func();
+   
+  console.log(result);
+ //undefined  
